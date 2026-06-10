1c15a0db1520fbc33f9c561f3f6feb1f6b12865d
n-yamamoto <n-yamamoto@sensekai.com>
2026-06-10 18:38:00 +0900

    feat: ダミーデータを作成できるように（TaskFactory）

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    // Task::factory()を使えるようにする
+    use HasFactory;
     protected $fillable = ['title', 'is_done', 'due_date', 'memo']; // Strong Parameterのような感じ（受け付けるリクエストの要素）
 }
