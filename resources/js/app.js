e40cbd5bd11204925e394b602e554de5963b5e7d
n-yamamoto <n-yamamoto@sensekai.com>
2026-06-10 16:52:40 +0900

    refactor: app.js を整理

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,31 @@ import $ from 'jquery'
 window.$ = $;
 window.jQuery = $;
 
+function openTaskModal() {
+    $('#task-modal').removeClass('hidden');
+}
+
+function closeTaskModal() {
+    $('#task-modal').addClass('hidden');
+}
+
+function setTaskModalValues(title, dueDate, memo) {
+    $('#modal-task-title').val(title);
+    $('#modal-task-due-date').val(dueDate);
+    $('#modal-task-memo').val(memo);
+}
+
+function setTaskFormActions(id) {
+    $('#modal-task-form').attr('action', '/tasks/' + id);
+    $('#modal-task-delete-form').attr('action', '/tasks/' + id);
+}
+
+function submitTaskDeleteForm() {
+    if (confirm('このタスクを削除しますか？')) {
+        $('#modal-task-delete-form').submit();
+    }
+}
+
 $(function () {
     
     //  モーダル表示とタスク情報の取得
@@ -13,36 +38,29 @@ $(function () {
         const dueDate = $(this).data('due-date')
         const memo = $(this).data('memo')
 
-        $('#modal-task-title').val(title);
-        $('#modal-task-due-date').val(dueDate);
-        $('#modal-task-memo').val(memo);
-
-        // フォームの送り先
-        $('#modal-task-form').attr('action', '/tasks/' + id);
-        $('#modal-task-delete-form').attr('action', '/tasks/' + id);
+        setTaskModalValues(title, dueDate, memo);  // モーダルの中身にタスクの値をセット
+        setTaskFormActions(id);   // フォームの送り先をセット
         
-        $('#task-modal').removeClass('hidden');
+        openTaskModal();
     });
 
     // モーダル内削除
     $('#js-delete-task-form-modal').on('click', function () {
-        if (confirm('このタスクを削除しますか？')) {
-            $('#modal-task-delete-form').submit();
-        }
+        submitTaskDeleteForm();
     });
 
     // モーダルを閉じる
     $('#js-close-task-modal').on('click', function () {
-        $('#task-modal').addClass('hidden');
+        closeTaskModal();
     });
 
     $('#js-cancel-task-modal').on('click', function () {
-        $('#task-modal').addClass('hidden');
+        closeTaskModal();
     });
 
     $('#task-modal-backdrop').on('click', function (event) {
         if (event.target === this) {     // モーダルの黒い背景をクリックした時
-            $('#task-modal').addClass('hidden');
+            closeTaskModal();
         }
     });
 });
