e508c16ac1ee77d711d0570dc20f64d0a7dc76d7
n-yamamoto <n-yamamoto@sensekai.com>
2026-06-16 17:51:49 +0900

    refactor: TaskGroupController->show()のロジックをServiceに移行

--- app/Http/Controllers/TaskGroupController.php
+++ app/Http/Controllers/TaskGroupController.php
@@ -5,9 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\TaskGroupRequest;
 use App\Models\TaskGroup;
+use App\Contents\TaskGroup\TaskGroupShowService;
 
 class TaskGroupController extends Controller
 {
+    private TaskGroupShowService $showService;
+
+    public function __construct(TaskGroupShowService $showService)
+    {
+        $this->showService = $showService;
+    }
+
     public function index()
     {
         $taskGroups = TaskGroup::latest()->get();
@@ -16,29 +24,8 @@ class TaskGroupController extends Controller
 
     public function show(TaskGroup $taskGroup, Request $request)
     {
-        // フィルター機能
-        $filter = $request->query("filter", "all");
-        $query = $taskGroup->tasks()->with('taskGroup');
-
-        if ($filter === 'active') {
-            $query->where('is_done', false);
-        }
-
-        if ($filter === 'completed') {
-            $query->where('is_done', true);
-        }
-
-        // 完了ステータス順 > 新規順 に並び替えて実際に取得
-        $tasks = $query
-            ->orderBy('is_done')
-            ->orderByRaw('due_date IS NULL')
-            ->orderBy('due_date')
-            ->latest()
-            ->get();
-
-        $taskGroups = TaskGroup::orderBy('name')->get();
-
-        return view('task-groups.show', compact('taskGroup', 'tasks', 'filter', 'taskGroups'));
+        $data = $this->showService->execute($taskGroup, $request);
+        return view('task-groups.show', $data);
     }
 
     public function store(TaskGroupRequest $request)
