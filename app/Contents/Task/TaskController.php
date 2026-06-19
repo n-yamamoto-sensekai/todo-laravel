acc0e93272909cc97971e25703f56cbfb3c2a3cc
n-yamamoto <n-yamamoto@sensekai.com>
2026-06-19 18:12:25 +0900

    refactor: TaskControoler->index()のロジックをServiceに移行

--- app/Contents/Task/TaskController.php
+++ app/Contents/Task/TaskController.php
@@ -3,40 +3,23 @@ namespace App\Contents\Task;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Contents\Task\TaskRequest;
 use App\Models\Task;
-use App\Models\TaskGroup;
 
 class TaskController extends Controller
 {
+    private TaskIndexRetrieveService $indexRetrieveService;
+
+    public function __construct(
+        TaskIndexRetrieveService $indexRetrieveService
+    ) {
+        $this->indexRetrieveService = $indexRetrieveService;
+    }
+
     // 一覧表示
     public function index(Request $request)
     {
-        // フィルター機能
-        $filter = $request->query("filter", "all");  // URLにfilterがない場合allを使う
-
-        $query = Task::with('taskGroup'); // Taskテーブルからデータを取得する準備をする（N+1問題対策に紐づくtaskGroupを一緒に取得）
-
-        if ($filter === 'active') {
-            $query->where('is_done', false);  // 条件で絞り込む
-        }
-
-        if ($filter === 'completed') {
-            $query->where('is_done', true);
-        }
-
-        // 完了ステータス順 > 新規順 に並び替えて実際に取得
-        $tasks = $query
-            ->orderBy('is_done')
-            ->orderByRaw('due_date IS NULL') // 期限がない => 1(true), ある => 0(false)
-            ->orderBy('due_date')
-            ->latest()
-            ->get();
-
-        // モーダルのグループ選択肢用
-        $taskGroups = TaskGroup::orderBy('name')->get();
-
-        return view("tasks.index", compact("tasks", "filter", "taskGroups"));
+        $data = $this->indexRetrieveService->execute($request);
+        return view("tasks.index", $data);
     }
 
     // 追加
