424d056008db0a1d4ef351afc96d43044534abac
n-yamamoto <n-yamamoto@sensekai.com>
2026-06-26 13:58:26 +0900

    ui: 編集ページのUIを調整

--- resources/views/task-groups/edit.blade.php
+++ resources/views/task-groups/edit.blade.php
@@ -1,18 +1,13 @@
 @extends('layouts.app')
 @section('title', 'タスクグループ編集')
 @section('content')
-    <div class="mb-6">
-        <a
-            href="{{ route('task-groups.index') }}"
-            class="text-blue-600 hover:underline"
-        >
-            タスクグループ一覧へ戻る
-        </a>
-    </div>
+    <div class="flex justify-between items-center mb-6">
+        <h1 class="text-3xl font-bold text-blue-600">タスクグループ編集</h1>
 
-    <h1 class="text-2xl font-bold mb-6">
-        タスクグループ編集
-    </h1>
+        <x-normal-link href="{{ route('task-groups.index') }}">
+            グループ一覧へ戻る →
+        </x-normal-link>
+    </div>
 
     <form
         action="{{ route('task-groups.update', $taskGroup) }}"
@@ -21,20 +16,22 @@
     >
         @csrf
         @method('PUT')
-        <div>
-            <label class="block mb-1 font-semibold">
-                グループ名
-            </label>
+        <label class="block mb-1">
+            グループ名
+        </label>
+        <div class="flex justify-between gap-2">
             <input
                 type="text"
                 name="name"
                 value="{{ old('name', $taskGroup->name) }}"
-                class="border rounded px-3 py-2 w-full"
+                class="border rounded px-3 py-2 flex-1"
             />
-            <x-input-error name="name" />
+            <x-primary-button>
+                更新
+            </x-primary-button>
         </div>
-        <x-primary-button>
-            更新
-        </x-primary-button>
+        
+        <x-input-error name="name" />
+
     </form>
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,53 +3,52 @@
 @section('title', 'タスク編集')
 
 @section('content')
-        <h1 class="text-3xl font-bold text-blue-600 mb-6">タスク編集</h1>
+    <div class="flex justify-between items-center mb-6">
+        <h1 class="text-3xl font-bold text-blue-600">タスク編集</h1>
 
-        <form action="{{ route('tasks.update', $task) }}" method="POST" class="mb-6">
-            @csrf
-            @method('PUT') {{-- LaravelにPUTリクエストとして送信 --}}
+        <x-normal-link href="{{ route('tasks.index') }}">
+            一覧に戻る →
+        </x-normal-link>
+    </div>
 
-            <div class="flex gap-2">
+    <form action="{{ route('tasks.update', $task) }}" method="POST" class="mb-6">
+        @csrf
+        @method('PUT') {{-- LaravelにPUTリクエストとして送信 --}}
 
-                <x-text-input name="title" :value="old('title', $task->title)" />
+        <x-text-input name="title" :value="old('title', $task->title)" />
 
+        <x-input-error name="title" />
+
+            <div class="mt-4">
+                <label class="block text-sm font-medium text-gray-700">
+                    期限
+                </label>
+                <input
+                    type="date"
+                    name="due_date"
+                    value="{{ old('due_date', $task->due_date) }}"
+                    class="mt-1 w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >
+                <x-input-error name="due_date"/>
+            </div>
+
+            <div class="mt-4">
+                <label class="block text-sm font-medium text-gray-700">
+                    メモ
+                </label>
+                <textarea
+                    name="memo"
+                    rows="4"
+                    class="mt-1 w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >{{ old('memo', $task->memo) }}</textarea>
+
+                <x-input-error name="memo"/>
+            </div>
+
+            <div class="mt-4 w-max ml-auto">
                 <x-primary-button>
                     更新
                 </x-primary-button>
-
             </div>
-
-            <x-input-error name="title" />
-
-                <div class="mt-4">
-                    <label class="block text-sm font-medium text-gray-700">
-                        期限
-                    </label>
-                    <input
-                        type="date"
-                        name="due_date"
-                        value="{{ old('due_date', $task->due_date) }}"
-                        class="mt-1 w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    >
-                    <x-input-error name="due_date"/>
-                </div>
-
-                <div class="mt-4">
-                    <label class="block text-sm font-medium text-gray-700">
-                        メモ
-                    </label>
-                    <textarea
-                        name="memo"
-                        rows="4"
-                        class="mt-1 w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    >{{ old('memo', $task->memo) }}</textarea>
-
-                    <x-input-error name="memo"/>
-                </div>
-        </form>
-
-        <a href="{{ route('tasks.index') }}" class="text-blue-600 hover:underline">
-            一覧に戻る
-        </a>
-    </div>
+    </form>
 @endsection
